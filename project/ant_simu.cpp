1f54ef3f3045d1fe70722e2439e3dec70f8fe0a3
Ric <ricardorico0527@gmail.com>
2020-01-15 23:30:11 +0100

    update

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -33,7 +33,7 @@ void advance_time( const labyrinthe& land, pheromone& phen,
 {
     start[1] = std::chrono::system_clock::now();
 
-    #pragma omp parallel for schedule(dynamic,64) reduction(+:cpteur)
+    //#pragma omp parallel for schedule(dynamic,64) reduction(+:cpteur)
     for ( size_t i = 0; i < ants.size(); ++i ){
         ants[i].advance(phen, land, pos_food, pos_nest, cpteur);    
     }
